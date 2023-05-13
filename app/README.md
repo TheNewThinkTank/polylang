c34cb02fe68ab26bd20fee983a27ae1f493b9da1
Gustav Rasmussen <gcr84@hotmail.com>
2023-05-13 17:06:06 +0200

    chore: setup package structure

---
+++ app/README.md
@@ -0,0 +1,15 @@
+# polylang
+
+A package used for multi language learning
+
+# How to use
+After installing the package use following import:
+
+```Python
+import polylang
+```
+
+Then use following commands:
+
+```Python
+```
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("app/Readme.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="polylang",
+    version="0.0.1",
+    description="Multi language learning app",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TheNewThinkTank/polylang",
+    author="TheNewThinkTank",
+    author_email="gcr84@hotmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.11",
+)
